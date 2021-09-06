0476de62a476f182d872a637a0377519ff5fe4da
COBRAPAPAXD <87162927+820be03u38@users.noreply.github.com>
2021-09-06 09:36:31 +0530

    Update config.py

--- config.py
+++ config.py
@@ -12,7 +12,7 @@ API_ID = int(getenv("API_ID"))
 API_HASH = getenv("API_HASH")
 
 DURATION_LIMIT = int(getenv("DURATION_LIMIT", "60"))
-
+BOT_USERNAME = getenv("BOT_USERNAME", "EsportRobot")
 COMMAND_PREFIXES = list(getenv("COMMAND_PREFIXES", "/ !").split())
 
 SUDO_USERS = list(map(int, getenv("SUDO_USERS").split()))
